1462c89d822144b1f0b259f6dfae44107921775f
EsGui <determinationfullstack@gmail.com>
2025-10-28 22:01:33 -0300

    Estrutura sequencial - exercicio 10 feito

---
+++ estrutura-sequencial/ex010.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+Faça um programa que peça a temperatura em graus Celsius, e transforme em graus Fahrenheit
+
+*/
+
+
+$c = floatval(readline("Digite um valor em Celsius: "));
+
+$f = ($c * 9/5) + 32;
+
+echo "conversão = ".$f
+
+
+?>
