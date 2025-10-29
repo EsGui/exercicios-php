c43e977ee55497969fafbe16b56c21ea5e48188b
EsGui <determinationfullstack@gmail.com>
2025-10-28 21:29:24 -0300

    Estrutura sequencial - exercicio 4 feito

--- estrutura-sequencial/ex004.php
+++ estrutura-sequencial/ex004.php
@@ -6,9 +6,14 @@ Faça um programa que peça as 4 notas bimestrais e mostre a média.
 
 */
 
-$dado = readline("Digite alguma coisa: ");
+$nota1 = floatval(readline("Digite a primeira nota: "));
+$nota2 = floatval(readline("Digite a segunda nota: "));
+$nota3 = floatval(readline("Digite a terceira nota: "));
+$nota4 = floatval(readline("Digite a quarta nota: "));
 
-echo $dado
+$media = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+
+echo "media = ".number_format($media, 2);
 
 
 ?>
