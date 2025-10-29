edb3fb0fa2b303c7d1393cae98552b2062fa59d3
EsGui <determinationfullstack@gmail.com>
2025-10-28 21:47:10 -0300

    Estrutura sequencial - exercicio 6 feito

---
+++ estrutura-sequencial/ex006.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+Faça um programa que peça o raio de um círculo, calcule e mostre sua área
+
+*/
+
+$raio = floatval(readline("Digite o raio: "));
+
+$area = ($raio ** 2) * 3.1415;
+
+echo "área ".$area
+
+?>
