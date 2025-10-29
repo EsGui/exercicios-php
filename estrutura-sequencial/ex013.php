688ff12f2dbe047bf06c5c99ce61dda1aba0160c
EsGui <determinationfullstack@gmail.com>
2025-10-28 22:15:47 -0300

    Estrutura sequencial - exercicio 13 feito

---
+++ estrutura-sequencial/ex013.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+Tendo como dados de entrada um arquivo em Gigabytes, construa um algoritmo que faça a conversão para Megabytes e Kilobytes, usando a seguinte fórmula
+
+    - Para Megabytes: Gigabytes * 1024
+    - Para Kilobytes: Gigaytes * 1024 * 1024
+
+Responda o tamanho do arquivo em Megabytes e o tamanho em Kilobytes.
+*/
+
+$arquivo = floatval(readline("arquivo: "));
+
+$mega_bytes = $arquivo * 1024;
+
+$kilo_bytes = $arquivo * 1024 * 2;
+
+echo "Conversão Megabytes = ".$mega_bytes."\n";
+echo "Conversão Kilobytes = ".$kilo_bytes."\n"
+
+?>
