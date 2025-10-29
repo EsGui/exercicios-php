28df6535b0c956d4b064175def0df79cd59ab268
EsGui <determinationfullstack@gmail.com>
2025-10-28 21:51:57 -0300

    Estrutura sequencial - exercicio 7 feito

---
+++ estrutura-sequencial/ex007.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+
+Faça um programa que calcule a área de um quadrado, em seguida mostre
+o dobro desta área para o usuário
+
+*/
+
+$lado = floatval(readline("Digite a base: "));
+
+$dobro = ($lado ** 2) * 2;
+
+echo "Dobro da área = ".$dobro
+
+?>
