b692e0421379983d66ee2f6f13be6520438a829a
EsGui <determinationfullstack@gmail.com>
2025-10-28 21:37:27 -0300

    Estrutura sequencial - exercicio 5 feito

---
+++ estrutura-sequencial/ex005.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+Faça um progrmaa que converta metros para centímetros
+
+*/
+
+$metros = floatval(readline("Digite um valor em metros: "));
+
+$centimetros = $metros * 100;
+
+echo $metros." metros convertido para centimetros ".$centimetros." centimetros";
+
+?>
